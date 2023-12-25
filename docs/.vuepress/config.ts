672b8354ed9c63c51b136d4840cd7661eb334975
ourandream <ouranhuang@outlook.com>
2023-12-25 22:18:24 +0800

    config: complete config

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,10 +2,10 @@
 
 module.exports = {
   // 网站 Title
-  title: "My Blog",
+  title: "Ourandream's Blog",
 
   // 网站描述
-  description: "This is my blog",
+  description: "This is ourandream's blog",
 
   // 网站语言
   locales: {
@@ -25,19 +25,19 @@ module.exports = {
     // 个人信息（没有或不想设置的，删掉对应字段即可）
     personalInfo: {
       // 昵称
-      nickname: "meteorlxy",
+      nickname: "ourandream",
 
       // 个人简介 (支持 HTML)
       description: "Happy Coding<br/>Happy Life",
 
       // 电子邮箱
-      email: "meteor.lxy@foxmail.com",
+      email: "ouranhuang@outlook.com",
 
       // 所在地
-      location: "Xi'an City, China",
+      location: "GuangZhou, China",
 
       // 组织
-      organization: "Xi'an Jiao Tong University",
+      organization: "碧蓝档案剧情站",
 
       // 头像
       // 设置为外部链接
@@ -49,92 +49,14 @@ module.exports = {
       sns: {
         // Github 帐号和链接
         github: {
-          account: "meteorlxy",
-          link: "https://github.com/meteorlxy",
-        },
-
-        // Facebook 帐号和链接
-        facebook: {
-          account: "meteorlxy.cn",
-          link: "https://www.facebook.com/meteorlxy.cn",
-        },
-
-        // LinkedIn 帐号和链接
-        linkedin: {
-          account: "meteorlxy",
-          link: "http://www.linkedin.com/in/meteorlxy",
-        },
-
-        // Twitter 帐号和链接
-        twitter: {
-          account: "meteorlxy_cn",
-          link: "https://twitter.com/meteorlxy_cn",
-        },
-
-        // 新浪微博 帐号和链接
-        weibo: {
-          account: "@焦炭君_Meteor",
-          link: "https://weibo.com/u/2039655434",
-        },
-
-        // 知乎 帐号和链接
-        zhihu: {
-          account: "meteorlxy.cn",
-          link: "https://www.zhihu.com/people/meteorlxy.cn",
-        },
-
-        // 豆瓣 帐号和链接
-        douban: {
-          account: "159342708",
-          link: "https://www.douban.com/people/159342708",
-        },
-
-        // Reddit 帐号和链接
-        reddit: {
-          account: "meteorlxy",
-          link: "https://www.reddit.com/user/meteorlxy",
-        },
-
-        // Medium 帐号和链接
-        medium: {
-          account: "meteorlxy.cn",
-          link: "https://medium.com/@meteorlxy.cn",
-        },
-
-        // Instagram 帐号和链接
-        instagram: {
-          account: "meteorlxy.cn",
-          link: "https://www.instagram.com/meteorlxy.cn",
-        },
-
-        // GitLab 帐号和链接
-        gitlab: {
-          account: "meteorlxy",
-          link: "https://gitlab.com/meteorlxy",
-        },
-
-        // Bitbucket 帐号和链接
-        bitbucket: {
-          account: "meteorlxy",
-          link: "https://bitbucket.org/meteorlxy",
-        },
-
-        // Docker Hub 帐号和链接
-        docker: {
-          account: "meteorlxy",
-          link: "https://hub.docker.com/u/meteorlxy",
-        },
-
-        // CSDN 帐号和链接
-        csdn: {
-          account: "",
-          link: "https://www.csdn.net/",
+          account: "ourandream",
+          link: "https://github.com/ourandream",
         },
 
         // 掘金 帐号和链接
         juejin: {
-          account: "meteorlxy",
-          link: "https://juejin.im/user/5c6fa9dde51d453fcb7baf09",
+          account: "ourandream",
+          link: "https://juejin.im/user/3565065937646490",
         },
       },
     },
@@ -144,7 +66,6 @@ module.exports = {
       // header 的背景，可以使用图片，或者随机变化的图案（geopattern）
       background: {
         // 使用图片的 URL，如果设置了图片 URL，则不会生成随机变化的图案，下面的 useGeo 将失效
-        url: "/assets/img/bg.jpg",
 
         // 使用随机变化的图案，如果设置为 false，且没有设置图片 URL，将显示为空白背景
         useGeo: true,
@@ -164,7 +85,7 @@ module.exports = {
 
       // 添加自定义 footer (支持 HTML)
       custom:
-        'Copyright 2018-present <a href="https://github.com/meteorlxy" target="_blank">meteorlxy</a> | MIT License',
+        'Copyright 2021-present <a href="https://github.com/ourandream" target="_blank">ourandream</a>',
     },
 
     // 个人信息卡片相关设置 (可选)
@@ -186,6 +107,7 @@ module.exports = {
     nav: [
       { text: "首页", link: "/", exact: true },
       { text: "文章", link: "/posts/", exact: false },
+      { text: "关于", link: "/about", exact: false },
     ],
 
     // 是否开启平滑滚动
@@ -198,10 +120,10 @@ module.exports = {
 
     // 评论配置，参考下方 [页面评论] 章节
     comments: {
-      owner: "meteorlxy",
-      repo: "vuepress-theme-meteorlxy",
-      clientId: "MY_CLIENT_ID",
-      clientSecret: "MY_CLIENT_SECRET",
+      owner: "ourandream",
+      repo: "vuepress-blog",
+      clientId: "9d964db2690f7b46cdcd",
+      clientSecret: "f4424dae8bb8f5a89eafeb059526667ab3e9977a",
     },
 
     // 分页配置 (可选)
